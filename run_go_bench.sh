72a0bec5b1969058a05e04115a7e290091dbe7db
8ugr4 <karadumanbugra@hotmail.com>
2025-01-16 16:20:07 +0100

    refactor: use grep to only get bench results

--- run_go_bench.sh
+++ run_go_bench.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 LOG_FILE="test.out"
-BENCHFILES_DIR="benchmark"
-go test -bench=. "$BENCHFILES_DIR"/* > $LOG_FILE
+go test ./... -bench=. -benchmem | grep -E '^Bench' > $LOG_FILE
 go run main.go
 # chmod 755 <script-name>.sh -> makes the text file executable
